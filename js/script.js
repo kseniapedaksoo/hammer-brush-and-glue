ad5d264b0e9366549ad78d29dc2b06c4ca6bcbbb
Ksenia P <ksenia.pedaksoo@gmail.com>
2023-08-25 00:21:02 +0100

    Swapped svg(s) to pause and play in Javascript

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@
 
   let menuOptions = document.querySelectorAll('nav > ul > li')
 
-  for(let i=0;i<menuOptions.length; i++){
+  for (let i = 0; i < menuOptions.length; i++) {
     menuOptions[i].onclick = function () {
       nav.classList.toggle("active");
     }
@@ -28,79 +28,79 @@
   /*
     Go to the top button
   */
-      // Get the button
-      let mybutton = document.getElementById("myBtn");
-        mybutton.addEventListener("click", topFunction)
+  // Get the button
+  let mybutton = document.getElementById("myBtn");
+  mybutton.addEventListener("click", topFunction)
 
-      // When the user scrolls down 20px from the top of the document, show the button
-      window.onscroll = function() {scrollFunction()};
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () { scrollFunction() };
 
-      function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          mybutton.style.display = "block";
-        } else {
-          mybutton.style.display = "none";
-          mybutton.classList.remove('can-touch-active')
-        }
-      }
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+      mybutton.classList.remove('can-touch-active')
+    }
+  }
 
-      // When the user clicks on the button, scroll to the top of the document
-      function topFunction(e) {
-        if(e.pointerType == undefined || !e.pointerType === 'mouse'){
-          //TOUCH
-          mybutton.classList.add('can-touch-active')
-          setTimeout(function(){
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-          }, 200)
-        }else{
-          //MOUSE
-          setTimeout(function(){
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-          }, 100)
-  
-        }
-       
-      }
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction(e) {
+    if (e.pointerType == undefined || !e.pointerType === 'mouse') {
+      //TOUCH
+      mybutton.classList.add('can-touch-active')
+      setTimeout(function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }, 200)
+    } else {
+      //MOUSE
+      setTimeout(function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }, 100)
+
+    }
+
+  }
 
 
   /* 
     Play - Pause button for video 
   */
-  
+
 
   let playPauseButton = document.getElementById("vidButton");
-    playPauseButton.addEventListener("click", playPause);
-    let myVideo = document.getElementById("myVideo");
-    function playPause() { 
-        if (myVideo.paused) {
-            myVideo.play();
-            playPauseButton.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"40\" height=\"40\" viewBox=\"0 0 512 512\"><title>Play</title><circle cx=\"256\" cy=\"256\" r=\"192\" style=\"fill:#3a4887;stroke:#fff;stroke-miterlimit:10;stroke-width:32px\"/><polygon points=\"203 166 350 256 206 346 206 157\" style=\"fill:#fff;\"/></svg>";
-            }
-        else  {
-            myVideo.pause(); 
-            playPauseButton.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"40\" height=\"40\" viewBox=\"0 0 512 512\"><title>Pause</title><path d=\"M448,256c0-106-86-192-192-192S64,150,64,256s86,192,192,192S448,362,448,256Z\" style=\"fill:#3a4887;stroke:#fff;stroke-miterlimit:10;stroke-width:32px\"/><line x1=\"208\" y1=\"192\" x2=\"208\" y2=\"320\" style=\"fill:none;stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px\"/><line x1=\"304\" y1=\"192\" x2=\"304\" y2=\"320\" style=\"fill:none;stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px\"/></svg>";
-            }
-    } 
-  
+  playPauseButton.addEventListener("click", playPause);
+  let myVideo = document.getElementById("myVideo");
+  function playPause() {
+    if (myVideo.paused) {
+      myVideo.play();
+      playPauseButton.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"40\" height=\"40\" viewBox=\"0 0 512 512\"><title>Pause</title><path d=\"M448,256c0-106-86-192-192-192S64,150,64,256s86,192,192,192S448,362,448,256Z\" style=\"fill:#3a4887;stroke:#fff;stroke-miterlimit:10;stroke-width:32px\"/><line x1=\"208\" y1=\"192\" x2=\"208\" y2=\"320\" style=\"fill:none;stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px\"/><line x1=\"304\" y1=\"192\" x2=\"304\" y2=\"320\" style=\"fill:none;stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px\"/></svg>";
+    }
+    else {
+      myVideo.pause();
+      playPauseButton.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"40\" height=\"40\" viewBox=\"0 0 512 512\"><title>Play</title><circle cx=\"256\" cy=\"256\" r=\"192\" style=\"fill:#3a4887;stroke:#fff;stroke-miterlimit:10;stroke-width:32px\"/><polygon points=\"203 166 350 256 206 346 206 157\" style=\"fill:#fff;\"/></svg>";
+    }
+  }
+
 
 
   /* 
       Slider for the What Children say section
   */
-    slider("children-say")
+  slider("children-say")
 
- /* 
-      Slider for the Gallery section
-  */
-    slider("gallery")
+  /* 
+       Slider for the Gallery section
+   */
+  slider("gallery")
 
 
   /* 
       Why choose us slider
-  */   
-    slider("why-choose-us")
+  */
+  slider("why-choose-us")
 
 
 
@@ -138,38 +138,38 @@
       We are also using setTimeout to remove notification and bring back the ENQUIRE button
   */
 
-  document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function () {
     const enquireButton = document.getElementById('enquire-submit');
     const form = document.querySelector('.contact-form');
     const sentMessage = document.querySelector('.request-sent');
     const errorMessage = document.querySelector('.request-error');
 
-    form.addEventListener('submit', async function(event) {
-        event.preventDefault();
+    form.addEventListener('submit', async function (event) {
+      event.preventDefault();
 
-        const formData = new FormData(form);
-        const response = await fetch('send-mail.php', {
-            method: 'POST',
-            body: formData
-        });
-      
-        const jsonResponse = await response.json();
+      const formData = new FormData(form);
+      const response = await fetch('send-mail.php', {
+        method: 'POST',
+        body: formData
+      });
 
-        if (jsonResponse.is_email_sent) {
-            enquireButton.style.display = 'none'
-            sentMessage.style.display = 'block';
-        } else {
-            enquireButton.style.display = 'none'
-            errorMessage.style.display = 'block';
-        }
-        setTimeout(function (){
-            enquireButton.removeAttribute('style')
-            sentMessage.style.display = 'none';
-            errorMessage.style.display = 'none';
-        }, 3000)
+      const jsonResponse = await response.json();
+
+      if (jsonResponse.is_email_sent) {
+        enquireButton.style.display = 'none'
+        sentMessage.style.display = 'block';
+      } else {
+        enquireButton.style.display = 'none'
+        errorMessage.style.display = 'block';
+      }
+      setTimeout(function () {
+        enquireButton.removeAttribute('style')
+        sentMessage.style.display = 'none';
+        errorMessage.style.display = 'none';
+      }, 3000)
     });
-});
- 
+  });
+
 
 })();
 
