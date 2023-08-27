a59c49d33962f2a112da6678c994f0f76bb1dbce
Ksenia P <ksenia.pedaksoo@gmail.com>
2023-08-27 12:03:46 +0200

    renamed a class for log;  renamed class in php from 'class' to 'class-name' in the form

--- send-mail.php
+++ send-mail.php
@@ -4,7 +4,7 @@
     $childLastName = trim(stripslashes($_POST['child-last-name']));
     $childDob = trim(stripslashes($_POST['child-dob']));
     $gender = trim(stripslashes($_POST['gender']));
-    $class = trim(stripslashes($_POST['class']));
+    $class = trim(stripslashes($_POST['class-name']));
     $parentFirstName = trim(stripslashes($_POST['parent-first-name']));
     $parentLastName = trim(stripslashes($_POST['parent-last-name']));
     $yourEmail = trim(stripslashes($_POST['your-email']));
@@ -14,7 +14,7 @@
     $yourMessage = isset($_POST['your-message']) ? trim(stripslashes($_POST['your-message'])) : '';
 
 	$email_from = $yourEmail;
-	$email_to = 'gregpovorozniuk@gmail.com';
+	$email_to = 'ksenia.pedaksoo@gmail.com';
     $subject = 'New inquiry from ' . $email_from;
 	$body = 'Child name: ' . $childName . "\n\n" . 'Child last name: ' . $childLastName . "\n\n"  . 'Child date of birth: ' . $childDob . "\n\n" . 'Gender: ' . $gender . "\n\n" . 'Class name: ' . $class . "\n\n"  . 'Parent first name: ' . $parentFirstName  . "\n\n" . 'Parent last name: ' . $parentLastName . "\n\n" . 'Email address: ' . $yourEmail . "\n\n"  . 'Home address: ' . $homeAddress . "\n\n" . 'Mobile phone: ' . $mobilePhone . "\n\n" . 'Home phone: ' . $homePhone . "\n\n"  . 'Message: ' . $yourMessage;
 
@@ -26,4 +26,3 @@
     } else {
         echo json_encode(array("is_email_sent" => false));
     }
-
